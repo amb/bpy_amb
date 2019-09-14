52128fec1524eba937852d795203820c7efccf23
Tommi Hyppänen <thyppane@gmail.com>
2019-09-14 22:32:06 +0300

    License formatting changes

--- raycast.py
+++ raycast.py
@@ -1,20 +1,21 @@
-"""
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# -*- coding:utf-8 -*-
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Created Date: Thursday, August 8th 2019, 1:31:39 pm
+# Copyright: Tommi Hyppänen
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-Created Date: Sunday, July 14th 2019, 1:10:18 pm
-Copyright: Tommi Hyppänen
-"""
 
 import mathutils.bvhtree as bvht
 import mathutils as mu
@@ -151,6 +152,7 @@ class Raycaster:
         # lrays//w < h
         # w % 8 == 0
 
+        # is this padding really necessary?
         w = (int(np.sqrt(len(rays) - 1)) // self.tile_size) * self.tile_size + self.tile_size
 
         assert w * w >= len(rays) and w % self.tile_size == 0
