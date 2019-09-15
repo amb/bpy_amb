83831412bbd1108035ae56dca25d89e7effe44f0
Tommi Hyppänen <thyppane@gmail.com>
2019-09-15 15:57:56 +0300

    Image compression channels lower bits

--- image.py
+++ image.py
@@ -6,19 +6,18 @@ import numpy as np
 def img_compress(img):
     """ Compresses RGBA image array from Numpy into a string """
     if img.shape[0] > 32 or img.shape[1] > 32 or img.shape[0] < 8 or img.shape[1] < 8:
-        print("Wrong image size.")
+        print("Wrong image size. Required: width = [8, 32], height = [8, 32]")
         return None
     icon = []
     flatdim = img.shape[0] * img.shape[1]
     for v in image.reshape((flatdim, 4)):
-        icon.append(int(round(v[0] * 31.0)))
-        icon.append(int(round(v[1] * 31.0)))
-        icon.append(int(round(v[2] * 31.0)))
-        icon.append(int(round(v[3] * 31.0)))
+        icon.append(int(round(v[0] * 15.0)))
+        icon.append(int(round(v[1] * 15.0)))
+        icon.append(int(round(v[2] * 7.0)))
+        icon.append(int(round(v[3] * 3.0)))
 
     compressed = zlib.compress(bytes(icon), level=6)
     encoded = base64.b64encode(compressed)
-    # print(len(icon), "=>", len(compressed))
 
     return repr(img.shape[0]) + "," + repr(img.shape[1]) + "," + encoded.decode("utf-8")
 
@@ -30,7 +29,10 @@ def img_decompress(inp):
     decoded = base64.b64decode(vals[2])
     uncompressed = zlib.decompress(decoded)
     values = []
-    for v in uncompressed:
-        values.append(v / 31)
+    for v in range(len(uncompressed))[::4]:
+        values.append(uncompressed[v + 0] / 15)
+        values.append(uncompressed[v + 1] / 15)
+        values.append(uncompressed[v + 2] / 7)
+        values.append(uncompressed[v + 3] / 3)
 
     return np.array(values).reshape((w, h, 4))
