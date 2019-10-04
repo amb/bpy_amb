4ad188d5add6bc84d1b18e0573f2466ca287d462
Tommi Hyppänen <thyppane@gmail.com>
2019-10-04 08:29:31 +0300

    Image funcs

--- image.py
+++ image.py
@@ -20,6 +20,61 @@ def linear2srgb(c):
     return srgb
 
 
+def add_material(name, image):
+    if len(name) > 60:
+        name = name[:60]
+    if name not in bpy.data.materials.keys():
+        mat = bpy.data.materials.new(name)
+        mat.use_nodes = True
+        sn = mat.node_tree.nodes.new("ShaderNodeTexImage")
+        bc = mat.node_tree.nodes["Principled BSDF"].inputs["Base Color"]
+        mat.node_tree.links.new(sn.outputs["Color"], bc)
+    else:
+        mat = bpy.data.materials[name]
+
+    mat.node_tree.nodes["Image Texture"].image = image
+    return mat
+
+
+def gl_copy(image):
+    if image.gl_load():
+        raise Exception()
+
+    width, height = image.size
+    offscreen = gpu.types.GPUOffScreen(width, height)
+
+    with offscreen.bind():
+        bgl.glClear(bgl.GL_COLOR_BUFFER_BIT)
+        with gpu.matrix.push_pop():
+            # reset matrices -> use normalized device coordinates [-1, 1]
+            gpu.matrix.load_matrix(mu.Matrix.Identity(4))
+            gpu.matrix.load_projection_matrix(mu.Matrix.Identity(4))
+
+            shader = gpu.shader.from_builtin("2D_IMAGE")
+            batch = batch_for_shader(
+                shader,
+                "TRI_FAN",
+                {
+                    "pos": ((0, 0), (width - 1, 0), (width - 1, height - 1), (0, height - 1)),
+                    "texCoord": ((0, 0), (1, 0), (1, 1), (0, 1)),
+                },
+            )
+
+            bgl.glActiveTexture(bgl.GL_TEXTURE0)
+            bgl.glBindTexture(bgl.GL_TEXTURE_2D, image.bindcode)
+
+            shader.bind()
+            shader.uniform_int("image", 0)
+            batch.draw(shader)
+
+        buffer = bgl.Buffer(bgl.GL_BYTE, width * height * 4)
+        bgl.glReadBuffer(bgl.GL_BACK)
+        bgl.glReadPixels(0, 0, width, height, bgl.GL_RGBA, bgl.GL_UNSIGNED_BYTE, buffer)
+
+    offscreen.free()
+    return np.array(buffer.to_list()).astype(np.float).reshape((width, height, 4)) / 255.0
+
+
 def rgb2hsv(image):
     tmp = image[:, :, :3]
     r = tmp[:, :, 0]
