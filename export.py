1665e6f02f8ebb7bc6d7f23b7a3d9793c13793ed
Tommi Hyppänen <thyppane@gmail.com>
2019-10-29 18:20:03 +0200

    Add export functionality

---
+++ export.py
@@ -0,0 +1,33 @@
+import re
+import os
+
+addon_name = None
+
+try:
+    print("Current zip version:", [i for i in os.listdir("..") if i.endswith(".zip")][0])
+except IndexError:
+    print(".zip doesn't exist yet")
+
+with open("__init__.py", "r") as f:
+    data = f.read()
+    m = re.search('"version": (?P<version>\(.+\))', data)
+    version_number = tuple(m.group("version")[1:-1].split(", "))
+    m = re.search('"name": "(?P<name>.+)"', data)
+    addon_name = "".join(m.group("name"))
+
+    # result = eval(compile(data, '', 'exec'))
+    # print(dir(result))
+
+
+addon_name = addon_name.replace(" ", "_")
+version_number = ".".join(version_number)
+print("name: {}, version:{}".format(addon_name, version_number))
+command = (
+    "7z a {}_{}.zip ."
+    ' -xr!"__pycache__"'
+    ' -xr!"export"'
+    ' -xr!".*"'
+    ' -xr!"*.bat"'
+    ' -xr!"*.txt"'
+    ' -xr!"*.zip"'.format(addon_name, version_number)
+)
